a212709507dbae1679afcc6af8c8335ce6e2fc67
Ilaz Gashi <119267734+iligashi@users.noreply.github.com>
2025-04-18 00:30:52 +0200

    74 Webpage and Management system made with React and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffbe33",
+        secondary: "#222831",
+        dark: "#1a1a1a",
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        cursive: ['Dancing Script', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+} 
